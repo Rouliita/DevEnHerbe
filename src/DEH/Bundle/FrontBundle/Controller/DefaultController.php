149b16ad5c4ec0e28bde9ea48534cf532002858b
Julia Werkoff <jwerkoff@gmail.com>
2015-09-13 16:01:16 +0200

    add the project to github

---
+++ src/DEH/Bundle/FrontBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DEH\Bundle\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('DEHFrontBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/DEH/Bundle/FrontBundle/DEHFrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DEH\Bundle\FrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DEHFrontBundle extends Bundle
+{
+}
